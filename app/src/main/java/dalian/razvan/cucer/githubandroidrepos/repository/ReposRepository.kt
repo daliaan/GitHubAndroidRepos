fb2a365f162bbff6b63380716b06b0bf2faeb23d
cucer razvan <razvan@kuwaitnet.com>
2021-02-15 21:03:23 +0200

    repos repository implementation done

--- app/src/main/java/dalian/razvan/cucer/githubandroidrepos/repository/ReposRepository.kt
+++ app/src/main/java/dalian/razvan/cucer/githubandroidrepos/repository/ReposRepository.kt
@@ -1,4 +1,16 @@
 package dalian.razvan.cucer.githubandroidrepos.repository
 
+import dalian.razvan.cucer.core.data.network.Result
+import dalian.razvan.cucer.githubandroidrepos.models.Repo
+import dalian.razvan.cucer.githubandroidrepos.network.model.ReposResponse
+
 interface ReposRepository {
+
+    suspend fun loadRepos(): Result<ReposResponse?>
+
+    fun setRepos(list: ArrayList<Repo>)
+    fun getRepos(): ArrayList<Repo>
+
+    fun setSelectedRepo(repo: Repo?)
+    fun getSelectedRepo(): Repo?
 }
--- app/src/main/java/dalian/razvan/cucer/githubandroidrepos/repository/ReposRepositoryImpl.kt
+++ app/src/main/java/dalian/razvan/cucer/githubandroidrepos/repository/ReposRepositoryImpl.kt
@@ -1,6 +1,27 @@
 package dalian.razvan.cucer.githubandroidrepos.repository
 
+import dalian.razvan.cucer.core.data.network.Result
+import dalian.razvan.cucer.githubandroidrepos.models.Repo
 import dalian.razvan.cucer.githubandroidrepos.network.API
+import dalian.razvan.cucer.githubandroidrepos.network.model.ReposResponse
+import dalian.razvan.cucer.githubandroidrepos.network.safeApiCall
 
 class ReposRepositoryImpl(private val api: API): ReposRepository {
+
+    private val repos = arrayListOf<Repo>()
+    private var selectedRepo: Repo? = null
+    private var currentPageNumber = 1
+
+    override suspend fun loadRepos(): Result<ReposResponse?> = safeApiCall { api.getRepos(currentPageNumber) }
+
+    override fun setRepos(list: ArrayList<Repo>) {
+        repos.addAll(list)
+        if (list.size > 0) currentPageNumber++
+    }
+    override fun getRepos(): ArrayList<Repo> = repos
+
+    override fun setSelectedRepo(repo: Repo?) {
+        selectedRepo = repo
+    }
+    override fun getSelectedRepo(): Repo? = selectedRepo
 }
